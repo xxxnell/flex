06509c0dcede5ef08236b2cca41e2b133b56ad07
sinowl <sinow@yonsei.ac.kr>
2017-09-19 01:17:52 +0900

    + ListCounterSpec update test

--- scope-sketch-core/src/test/scala/sketch/scope/counter/ListCounterSpec.scala
+++ scope-sketch-core/src/test/scala/sketch/scope/counter/ListCounterSpec.scala
@@ -8,13 +8,24 @@ import org.specs2.ScalaCheck
   * Licensed by Probe Technology, Inc.
   */
 class ListCounterSpec extends Specification with ScalaCheck {
-
-  "ListCounter" in todo
-
+  
+    "ListCounter" should {
+      "update" in {
+        (for {
+            listcounter <- ListCounterGen.listCounterSample
+            updated <- ListCounter.update(listcounter, 10, 20.0)
+        } yield updated)
+          .fold(ko)( listcounter => ok )
+      }
+    }
 }
 
 object ListCounterGen {
 
-  def listCounterGen: Gen[ListCounter] = ???
+  def listCounterGen: Gen[ListCounter] = for {
+    size <- Gen.choose(30, 40)
+  } yield ListCounter.empty(size)
+
+  def listCounterSample: Option[ListCounter] = listCounterGen.sample
 
 }
