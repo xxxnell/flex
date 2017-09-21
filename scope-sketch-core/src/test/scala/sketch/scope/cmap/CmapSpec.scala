f4ad5c757204567dfe62933cb5ec5a1bc4ba7420
sinowl <sinow@yonsei.ac.kr>
2017-09-21 14:05:00 +0900

    + CmapSpec size test

--- scope-sketch-core/src/test/scala/sketch/scope/cmap/CmapSpec.scala
+++ scope-sketch-core/src/test/scala/sketch/scope/cmap/CmapSpec.scala
@@ -3,6 +3,7 @@ package sketch.scope.cmap
 import org.scalacheck.{Arbitrary, Gen}
 import org.specs2.mutable._
 import org.specs2.ScalaCheck
+//import sketch.scope.cmap.Cmap
 
 /**
   * Licensed by Probe Technology, Inc.
@@ -11,16 +12,33 @@ class CmapSpec extends Specification with ScalaCheck {
 
   "Cmap" should {
 
-    todo
+    "bin" in {
+      todo
+    }
+
+    "size" in {
+      implicit val cmapGen = CmapGen.cmapA
+
+      prop { (cmapTupple: (Int, Cmap) ) =>
+        if( cmapTupple._1+1 == cmapTupple._2.size) ok
+        else ko
+      }.setArbitrary(cmapGen)
+    }
+
+    "range" in {
+      todo
+    }
 
   }
 
 }
 
 object CmapGen {
+  
+  def cmapGen: Gen[(Int, Cmap)] = for {
+    n <- Gen.choose(1, 10)
+  } yield (n, Cmap.uniform(n))
 
-  def cmapGen: Gen[Cmap] = UniformCmapGen.uniformCmapGen
-
-  def cmapA: Arbitrary[Cmap] = Arbitrary(cmapGen)
+  def cmapA: Arbitrary[(Int, Cmap)] = Arbitrary(cmapGen)
 
 }
