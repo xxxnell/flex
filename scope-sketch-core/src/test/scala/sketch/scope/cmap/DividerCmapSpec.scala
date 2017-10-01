82c79e2cf7ed1292aeb0f43b54dce73cfc8aff97
sinowl <sinow@yonsei.ac.kr>
2017-10-02 00:41:16 +0900

    + DividerCmapSpec divider2InverseIndexingMap test

--- scope-sketch-core/src/test/scala/sketch/scope/cmap/DividerCmapSpec.scala
+++ scope-sketch-core/src/test/scala/sketch/scope/cmap/DividerCmapSpec.scala
@@ -21,7 +21,7 @@ class DividerCmapSpec extends Specification with ScalaCheck {
         val koMsg =
           s"Cannot get the recorded count: " +
             s"divider2indexMap size -> ${divider2indexMap.size}, divider size -> ${divider.size}, " +
-            s"indexmapHead -> ${divider2indexMap.get(1)}, indexmapTail -> ${divider2indexMap.get(divider.size)}"
+            s"indexmapHead -> ${divider2indexMap.get(divider(0))}, indexmapTail -> ${divider2indexMap.get(divider(divider.size-1))}"
 
         if( divider2indexMap.size == divider.size &&
             indexmapHead &&
@@ -31,7 +31,22 @@ class DividerCmapSpec extends Specification with ScalaCheck {
     }
 
     "divider2InverseIndexingMap" in {
-      todo
+      implicit val dividerGen = DividerCmapGen.dividerA
+
+      prop { (divider: (List[Double]) ) =>
+        val divider2inverseindexMap = DividerCmap.divider2InverseIndexingMap(divider)
+        val inverseindexmapHead = divider2inverseindexMap.get(0) == Some(divider(0))
+        val inverseindexmapTail = divider2inverseindexMap.get(divider.size-1) == Some(divider(divider.size-1))
+        val koMsg =
+          s"Cannot get the recorded count: " +
+            s"divider2indexMap size -> ${divider2inverseindexMap.size}, divider size -> ${divider.size}, " +
+            s"inverseindexmapHead -> ${divider2inverseindexMap.get(0)}, inverseindexmapTail -> ${divider2inverseindexMap.get(divider.size-1)}"
+
+        if( divider2inverseindexMap.size == divider.size &&
+          inverseindexmapHead &&
+          inverseindexmapTail ) ok
+        else ko(koMsg)
+      }.setArbitrary(dividerGen)
     }
 
     "bin" in {
