5aa52adef1dc501f7416403e8fd024a9a7ce371c
Namuk Park <namuk.park@gmail.com>
2017-12-02 15:53:18 +0900

    + implements the map of DistMonad

--- scope-sketch-core/src/main/scala/sketch/scope/pdf/monad/DistMonad.scala
+++ scope-sketch-core/src/main/scala/sketch/scope/pdf/monad/DistMonad.scala
@@ -7,11 +7,19 @@ import scala.language.higherKinds
 /**
   * Licensed by Probe Technology, Inc.
   */
-trait DistMonad[D1[_]<:Dist[_], D2[_]<:Dist[_], D3[_]<:Dist[_]] extends DistFunctor[D1] {
+trait DistMonad[D1[_]<:Dist[_], D2[_]<:Dist[_], D3[_]<:Dist[_]]
+  extends DistMonadLaws[D1, D2, D3]
+    with DistFunctor[D1] {
 
   def pure[A](measure: A => Prim, a: A): Dist[A] = DeltaDist(measure, measure(a))
 
   def bind[A, B](dist: D1[A], f: A => D2[B]): D3[B]
+
+}
+
+trait DistMonadLaws[D1[_]<:Dist[_], D2[_]<:Dist[_], D3[_]<:Dist[_]] { self: DistMonad[D1, D2, D3] =>
+
+  def map[A, B](dist: D1[A], f: A => B): D1[B] = bind(dist, a => pure(f(a)))
 
 }
 
