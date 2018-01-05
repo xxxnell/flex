00e225b580a0d07881dd27a274f6611329ef8f21
Namuk Park <namuk.park@gmail.com>
2018-01-05 15:17:28 +0900

    * add the exception handling for smoothingPsForEqualSpaceCumulative

--- scope-sketch-core/src/main/scala/sketch/scope/pdf/update/EqualSpaceCdfUpdate.scala
+++ scope-sketch-core/src/main/scala/sketch/scope/pdf/update/EqualSpaceCdfUpdate.scala
@@ -30,19 +30,20 @@ trait EqualSpaceCdfUpdate {
     val sorted = ps.sortBy(_._1)
     val sliding: List[List[(Prim, Count)]] = sorted.sliding(2).toList
     val headAppendingO: Option[(Prim, Count)] = sliding.headOption.flatMap {
-      case d1 :: d2 :: Nil => Some((d1._1 - (d2._1 - d1._1), 0d))
+      case (p1, _) :: (p2, _) :: Nil => Some((p1 - (p2 - p1), 0d))
       case _ => None
     }
     val lastAppendingO: Option[(Prim, Count)] = sliding.lastOption.flatMap {
-      case d1 :: d2 :: Nil => Some((d2._1 + (d2._1 - d1._1), 0d))
+      case (p1, _) :: (p2, _) :: Nil => Some((p2 + (p2 - p1), 0d))
       case _ => None
     }
 
-    val records = (headAppendingO.toList ::: sorted ::: lastAppendingO.toList).sliding(2).toList
+    val records = (headAppendingO.toList ::: sorted ::: lastAppendingO.toList)
+      .sliding(2).toList
       .flatMap {
-        case d1 :: d2 :: Nil =>
-          val range = RangeP(d1._1, d2._1)
-          if(!range.isPoint) Some((range, (d1._2 + d2._2) / (2 * range.length).toDouble)) else None
+        case (p1, count1) :: (p2, count2) :: Nil if !p1.isInfinity && !p2.isInfinity =>
+          val range = RangeP(p1, p2)
+          if(!range.isPoint) Some((range, (count1 + count2) / (2 * range.length).toDouble)) else None
         case _ => None
       }
 
