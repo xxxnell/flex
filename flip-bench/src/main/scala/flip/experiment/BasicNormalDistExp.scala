07ce77362fa7e15acb34fbf592d0508a965aa4a5
Nell <xxxxxnell@gmail.com>
2018-01-22 10:26:26 +0900

    + add cosine sim for BasicNormalDistExp * fix the order of ConsineSyntax inheritance

--- flip-bench/src/main/scala/flip/experiment/BasicNormalDistExp.scala
+++ flip-bench/src/main/scala/flip/experiment/BasicNormalDistExp.scala
@@ -8,7 +8,8 @@ object BasicNormalDistExp {
 
   def main(args: Array[String]): Unit = {
     val expName1 = "basic-normal"
-    val sampleNo = 1000
+    val dataNo = 1000
+    val samplingNo = 100
     val start = 50
     val period = 100
     val minDomainCutoff = -10e10
@@ -16,12 +17,12 @@ object BasicNormalDistExp {
 
     implicit val conf: SketchConf = SketchConf(
       startThreshold = start, thresholdPeriod = period, queueSize = 30,
-      cmapSize = 150, cmapNo = 5, cmapStart = Some(-10d), cmapEnd = Some(10),
+      cmapSize = samplingNo, cmapNo = 5, cmapStart = Some(-10d), cmapEnd = Some(10),
       counterSize = 1000, counterNo = 2
     )
     val sketch = Sketch.empty[Double]
     val underlying = NumericDist.normal(0.0, 1)
-    val (_, datas) = underlying.samples(sampleNo)
+    val (_, datas) = underlying.samples(dataNo)
     val dataIdxs = datas.zipWithIndex
 
     var tempSketchO: Option[Sketch[Double]] = Option(sketch)
@@ -30,7 +31,7 @@ object BasicNormalDistExp {
       tempSketchO.map(tempSketch => (idx + 1, tempSketch))
     }.filter { case (idx, _) => idx % 10 == 0 }
     val idxDensityPlots = idxUtdSketches.flatMap { case (idx, utdSkt) => utdSkt.densityPlot.map(plot => (idx, plot)) }
-    val idxKldPlot = idxUtdSketches.flatMap { case (idx, utdSkt) =>
+    val idxKldDensity = idxUtdSketches.flatMap { case (idx, utdSkt) =>
       for {
         sampling <- underlying.sampling(utdSkt, conf)
         filtered = sampling.filter { range => range > minDomainCutoff && range < maxDomainCutoff }
@@ -44,11 +45,27 @@ object BasicNormalDistExp {
         kld <- KLD(filtered, utdSkt)
       } yield (idx, kld)
     }
+    val idxCosineDensity = idxUtdSketches.flatMap { case (idx, utdSkt) =>
+      for {
+        sampling <- underlying.uniformSampling(-2.5, 2.5, samplingNo * 3)
+        filtered = sampling.filter { range => range > minDomainCutoff && range < maxDomainCutoff }
+        cosine <- CosineDensity(filtered, utdSkt)
+      } yield (idx, cosine)
+    }
+    val idsCosine = idxUtdSketches.flatMap { case (idx, utdSkt) =>
+      for {
+        sampling <- underlying.uniformSampling(-2.5, 2.5, samplingNo * 3)
+        filtered = sampling.filter { range => range > minDomainCutoff && range < maxDomainCutoff }
+        cosine <- Cosine(filtered, utdSkt)
+      } yield (idx, cosine)
+    }
 
     ExpOutOps.clear(expName1)
     ExpOutOps.writePlots(expName1, "pdf", idxDensityPlots)
-    ExpOutOps.writePlots(expName1, "kld-density", idxKldPlot)
+    ExpOutOps.writePlots(expName1, "kld-density", idxKldDensity)
     ExpOutOps.writeStr(expName1, "kld", idxKld.map{ case (idx, kld) => s"$idx, $kld" }.mkString("\n"))
+    ExpOutOps.writePlots(expName1, "cosine-density", idxCosineDensity)
+    ExpOutOps.writeStr(expName1, "cosine", idsCosine.map{ case (idx, cosine) => s"$idx, $cosine" }.mkString("\n"))
   }
 
 }
--- flip-core/src/main/scala/flip/sim/SimSyntax.scala
+++ flip-core/src/main/scala/flip/sim/SimSyntax.scala
@@ -14,11 +14,11 @@ trait CosineSyntax1 extends CosineSyntax2 {
 
   private val cosine = flip.sim.Cosine
 
-  def Cosine[A](d1: PlottedDist[A], d2: Dist[A])(implicit conf2: DistConf): Option[Double] =
-    cosine(d1, d2, conf2).simForPlotted(d1, d2, conf2)
+  def Cosine[A](d1: SamplingDist[A], conf1: SamplingDistConf, d2: Dist[A], conf2: DistConf): Option[Double] =
+    cosine(d1, conf1, d2, conf2).simForSampling(d1, conf1, d2, conf2)
 
-  def CosineDensity[A](d1: PlottedDist[A], d2: Dist[A])(implicit conf2: DistConf): Option[DensityPlot] =
-    cosine(d1, d2, conf2).simDensityForPlotted(d1, d2, conf2)
+  def CosineDensity[A](d1: SamplingDist[A], conf1: SamplingDistConf, d2: Dist[A], conf2: DistConf): Option[DensityPlot] =
+    cosine(d1, conf1, d2, conf2).simDensityForSampling(d1, conf1, d2, conf2)
 
 }
 
@@ -26,11 +26,11 @@ trait CosineSyntax2 {
 
   private val cosine = flip.sim.Cosine
 
-  def Cosine[A](d1: SamplingDist[A], conf1: SamplingDistConf, d2: Dist[A], conf2: DistConf): Option[Double] =
-    cosine(d1, conf1, d2, conf2).simForSampling(d1, conf1, d2, conf2)
+  def Cosine[A](d1: PlottedDist[A], d2: Dist[A])(implicit conf2: DistConf): Option[Double] =
+    cosine(d1, d2, conf2).simForPlotted(d1, d2, conf2)
 
-  def CosineDensity[A](d1: SamplingDist[A], conf1: SamplingDistConf, d2: Dist[A], conf2: DistConf): Option[DensityPlot] =
-    cosine(d1, conf1, d2, conf2).simDensityForSampling(d1, conf1, d2, conf2)
+  def CosineDensity[A](d1: PlottedDist[A], d2: Dist[A])(implicit conf2: DistConf): Option[DensityPlot] =
+    cosine(d1, d2, conf2).simDensityForPlotted(d1, d2, conf2)
 
 }
 
