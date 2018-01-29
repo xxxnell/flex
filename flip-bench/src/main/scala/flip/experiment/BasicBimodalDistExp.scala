975a0c13512478bc59942dbbbfed36be8053dba4
Nell <xxxxxnell@gmail.com>
2018-01-30 08:21:02 +0900

    * beautify BasicBimodalDistExp

--- flip-bench/src/main/scala/flip/experiment/BasicBimodalDistExp.scala
+++ flip-bench/src/main/scala/flip/experiment/BasicBimodalDistExp.scala
@@ -12,7 +12,15 @@ object BasicBimodalDistExp {
     val start = 50
     val period = 100
 
-    val sketch = initSketch(start, period, samplingNo)
+    val sketch = {
+      implicit val conf: SketchConf = SketchConf(
+        startThreshold = start, thresholdPeriod = period, boundaryCorr = 0.1, decayFactor = 0,
+        queueSize = 30,
+        cmapSize = samplingNo, cmapNo = 5, cmapStart = Some(-10d), cmapEnd = Some(10),
+        counterSize = samplingNo
+      )
+      Sketch.empty[Double]
+    }
     val underlying = (0.5, NumericDist.normal(-2.0, 1)) + (0.5, NumericDist.normal(2.0, 1))
     val (_, datas) = underlying.samples(dataNo)
     val dataIdxs = datas.zipWithIndex
@@ -27,10 +35,10 @@ object BasicBimodalDistExp {
       ComparisonOps.identicalDomain(underlying, utdSkt, KLD[Double]).map((idx, _))
     }
     val idxCosine = idxUtdSketches.flatMap { case (idx, utdSkt) =>
-      ComparisonOps.uniformDomain(underlying, -2.5, 2.5, samplingNo * 3, utdSkt, Cosine[Double]).map((idx, _))
+      ComparisonOps.identicalDomain(underlying, utdSkt, Cosine[Double]).map((idx, _))
     }
     val idxEuclidean = idxUtdSketches.flatMap { case (idx, utdSkt) =>
-      ComparisonOps.uniformDomain(underlying, -2.5, 2.5, samplingNo * 3, utdSkt, Euclidean[Double]).map((idx, _))
+      ComparisonOps.identicalDomain(underlying, utdSkt, Euclidean[Double]).map((idx, _))
     }
 
     ExpOutOps.clear(expName1)
@@ -38,16 +46,6 @@ object BasicBimodalDistExp {
     ExpOutOps.writeStr(expName1, "kld", idxKld.map { case (idx, kld) => s"$idx, $kld" }.mkString("\n"))
     ExpOutOps.writeStr(expName1, "cosine", idxCosine.map { case (idx, cosine) => s"$idx, $cosine" }.mkString("\n"))
     ExpOutOps.writeStr(expName1, "euclidean", idxEuclidean.map { case (idx, euc) => s"$idx, $euc" }.mkString("\n"))
-  }
-
-  def initSketch(start: Int, period: Int, samplingNo: Int): Sketch[Double] = {
-    implicit val conf: SketchConf = SketchConf(
-      startThreshold = start, thresholdPeriod = period, boundaryCorr = 0.1, decayFactor = 0,
-      queueSize = 30,
-      cmapSize = samplingNo, cmapNo = 5, cmapStart = Some(-10d), cmapEnd = Some(10),
-      counterSize = samplingNo
-    )
-    Sketch.empty[Double]
   }
 
 }
