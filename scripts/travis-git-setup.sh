44127a86469fcf172b321f92556d8d719fe40377
Nell <xxxxxnell@gmail.com>
2018-02-18 19:35:56 +0900

    * fix typo

--- scripts/travis-git-setup.sh
+++ scripts/travis-git-setup.sh
@@ -4,8 +4,10 @@ git remote set-branches origin '*'
 git fetch -vvv
 git remote -a
 
-if [ -n "$GITHUB_ACESS_TOKEN" ]; then
+if [ -n "$GITHUB_ACCESS_TOKEN" ]; then
   echo "Changing origin url."
   githut_path=xxxnell/flip.git
   git remote set-url origin https://${GITHUB_ACCESS_TOKEN}@github.com/${githut_path}
+else
+  echo "GITHUB_ACCESS_TOKEN doesn't exist."
 fi
