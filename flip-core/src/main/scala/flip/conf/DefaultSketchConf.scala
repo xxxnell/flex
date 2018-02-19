7f64058fed31d1370f909afc53a890e9c52abbe4
Nell <xxxxxnell@gmail.com>
2018-02-19 17:17:22 +0900

    * update default configuration

--- flip-core/src/main/scala/flip/conf/DefaultSketchConf.scala
+++ flip-core/src/main/scala/flip/conf/DefaultSketchConf.scala
@@ -12,7 +12,7 @@ trait DefaultSketchConf extends AdaPerSketchConf {
 
   val decayFactor: Double = 1
 
-  val startThreshold: Double = 100
+  val startThreshold: Double = 50
 
   val thresholdPeriod: Double = 100
 
@@ -20,7 +20,7 @@ trait DefaultSketchConf extends AdaPerSketchConf {
 
   lazy val bindSampling: Int = cmap.no
 
-  val cmap: UniformCmapConf = CmapConf.uniform(200, 3, Some(-1000d), Some(1000d))
+  val cmap: UniformCmapConf = CmapConf.uniform(20, 3, Some(-1000d), Some(1000d))
 
   val counter: CounterConf = CounterConf.apply(70, 2)
 
