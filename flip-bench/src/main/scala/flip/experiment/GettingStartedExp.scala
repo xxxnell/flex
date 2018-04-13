1a60345b069e71bff05d47f24d55232034fe780e
Nell <xxxxxnell@gmail.com>
2018-04-13 18:55:16 +0900

    + Add the practical uses to `GettingStartedExp`

--- flip-bench/src/main/scala/flip/experiment/GettingStartedExp.scala
+++ flip-bench/src/main/scala/flip/experiment/GettingStartedExp.scala
@@ -11,13 +11,21 @@ object GettingStartedExp {
 
     // update samples to sketch
     val sketch0 = Sketch.empty[Double]
-    val utdSketch = samples.foldLeft(sketch0) {
+    val sketch1 = samples.foldLeft(sketch0) {
       case (sketch, sample) => sketch.update(sample)
     }
 
-    // get probability for interval [0.0, 1.0]
-    println("result: " + utdSketch.probability(0.0, 1.0))
-    println("expected: " + underlying.probability(0.0, 1.0))
+    // probability for interval [0.0, 1.0]
+    println(
+      s"Estimated Pr(0.0 ≤ x ≤ 1.0): ${sketch1.probability(0.0, 1.0)}, " +
+      s"Expected Pr(0.0 ≤ x ≤ 1.0): ${underlying.probability(0.0, 1.0)}"
+    )
+
+    // median
+    println(s"Estimated median: ${sketch1.median}, expected median: 0.0")
+
+    // sample from sketch
+    println("Sample from sketch: " + sketch1.sample._2)
   }
 
 }
