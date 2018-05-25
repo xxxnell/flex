e14c31e8db86a7efbe8636c48d6434a2872fc63d
Nell <xxxxxnell@gmail.com>
2018-05-26 07:53:23 +0900

    + Improve the `get` performance of `HCounter`

--- flip-core/src/test/scala/flip/hcounter/UncompressedHCounterSpec.scala
+++ flip-core/src/test/scala/flip/hcounter/UncompressedHCounterSpec.scala
@@ -10,11 +10,11 @@ class UncompressedHCounterSpec extends Specification with ScalaCheck {
     "get" in {
 
       "basic" in {
-        val emptyCounter = HCounter.emptyUncompressed(100)
+        val counter0 = HCounter.emptyUncompressed(100)
         val datas = (0 until 100).map(i => (i, i * i)).toList
-        val utdCounter = datas
-          .foldLeft(emptyCounter) { case (accCounter, (idx, count)) => accCounter.update(idx, count)}
-        val cond = datas.forall { case (idx, count) => utdCounter.get(idx) == count }
+        val counter1 = datas
+          .foldLeft(counter0) { case (accCounter, (idx, count)) => accCounter.update(idx, count)}
+        val cond = datas.forall { case (idx, count) => counter1.get(idx) == count }
 
         if(!cond) ko("Somewhere HCounter collides.")
         else ok
