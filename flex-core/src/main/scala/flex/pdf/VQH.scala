b2df474aa511296740b0cf72ad3d9e4cc2160603
Nell <xxxxxnell@gmail.com>
2019-01-05 14:22:05 +0900

    + Implement `expUpdate` of `VQH`

--- flex-core/src/main/scala/flex/pdf/VQH.scala
+++ flex-core/src/main/scala/flex/pdf/VQH.scala
@@ -42,53 +42,68 @@ trait VQHOps {
     * @return (Updated VQH, new codebook-vectors, out codebook-vectors)
     * */
   def parUpdate(vqh: VQH, xps: List[(INDArray, Int, Float)]): (VQH, List[VQH#Codebook], List[VQH#Codebook]) = {
-    xps.foldRight((vqh, List.empty[VQH#Codebook], List.empty[VQH#Codebook])) {
-      case (xp, (vqh0, newcs0, outcs0)) =>
-        val (vqh1, newcs1, outcs1) = singleParUpdate(vqh0, xp)
-        (vqh1, newcs1 ++ newcs0, outcs1 ++ outcs0)
+    xps.foldLeft((vqh, List.empty[VQH#Codebook], List.empty[VQH#Codebook])) {
+      case ((_vqh, _newcs, _outcs), (x, a, w)) =>
+        val (vqh1, newcs1, outcs1) = parSearch(vqh, x, a).map { case (c, n, i) =>
+          lazy val cnew = c.zipWithIndex.map { case (cp, b) => if (a == b) x else diffusion(cp) }
+          singleUpdate(_vqh, c, n, i, cnew, w)
+        }.getOrElse(vqh, List.empty[VQH#Codebook], List.empty[VQH#Codebook])
+        (vqh1, newcs1 ++ _newcs, outcs1 ++ _outcs)
     }
   }
 
-  def singleParUpdate(vqh: VQH, xp: (INDArray, Int, Float)): (VQH, List[VQH#Codebook], List[VQH#Codebook]) = {
-    val (x, a, w) = xp
-    parSearch(vqh, x, a).fold((vqh, List.empty[VQH#Codebook], List.empty[VQH#Codebook])) {
-      case (c, n, i) =>
-        // Step A. Increase the count
-        val vqh1 = patch(vqh, i, (c, n + w))
-
-        // Step B. Add a new cookbook vector
-        val avgpi = 1 / vqh.k.toFloat
-        val (berp, p) = Bernoulli(sigmoid(n / vqh.ntot - avgpi), vqh.rng).sample
-        val (vqh2, cnews) = if (p == 1) {
-          val cnew = c.zipWithIndex.map { case (cp, b) => if (a == b) x else diffusion(cp) }
-          val _vqh = patch(vqh1, i, (c, n / 2))
-          (add(_vqh, (cnew, n / 2)), cnew :: Nil)
-          // TODO abruptly forget loop
-        } else (vqh1, Nil)
-
-        // Step C. Remove old cookbook vectors
-        val qtot = vqh2.cns.map { case (_, m) => sigmoid(avgpi - m / vqh2.ntot) }.sum
-        var (berq, q, cns1, cs2) = (berp, 0, Vector.empty[(VQH#Codebook, Float)], List.empty[VQH#Codebook])
-        for (i <- 0 to vqh2.cns.length) {
-          val _cns = vqh2.cns.apply(i)
-          val (_berq, _q) = Bernoulli(sigmoid(avgpi - _cns._2 / vqh2.ntot) / qtot, berq.rng).sample
-          berq = _berq
-          q = _q
-          if (q == 0) cns1 = cns1.+:(_cns) else cs2 = _cns._1 :: cs2
-        }
-
-        (VQH(cns1, cns1.map(_._2).sum, vqh2.k, berq.rng), cnews, cs2)
+  /**
+    * Update input vectors with its indices.
+    * @return (Updated VQH, new codebook-vectors, out codebook-vectors)
+    * */
+  def expUpdate(vqh: VQH, xs: List[(VQH#Codebook, Float)]): (VQH, List[VQH#Codebook], List[VQH#Codebook]) = {
+    xs.foldLeft((vqh, List.empty[VQH#Codebook], List.empty[VQH#Codebook])) {
+      case ((_vqh, _newcs, _outcs), (x, w)) =>
+        val (vqh1, newcs1, outcs1) = expSearch(vqh, x).map { case (c, n, i) =>
+          singleUpdate(_vqh, c, n, i, x, w)
+        }.getOrElse(vqh, List.empty[VQH#Codebook], List.empty[VQH#Codebook])
+        (vqh1, newcs1 ++ _newcs, outcs1 ++ _outcs)
     }
   }
 
-  def expUpdate(vqh: VQH, xs: List[(VQH#Codebook, Float)]): (VQH, List[VQH#Codebook], List[VQH#Codebook]) = ???
+  def singleUpdate(vqh: VQH,
+                   c: VQH#Codebook, n: Float, i: Int,
+                   cnew: => VQH#Codebook, w: Float): (VQH, List[VQH#Codebook], List[VQH#Codebook]) = {
+    // Step A. Increase the count
+    val vqh1 = patch(vqh, i, (c, n + w))
+
+    // Step B. Add a new cookbook vector
+    val avgpi = 1 / vqh.k.toFloat
+    val (berp, p) = Bernoulli(sigmoid(n / vqh.ntot - avgpi), vqh.rng).sample
+    val (vqh2, cnews) = if (p == 1) {
+      val _vqh = patch(vqh1, i, (c, n / 2))
+      (add(_vqh, (cnew, n / 2)), cnew :: Nil)
+      // TODO abruptly forget loop
+    } else (vqh1, Nil)
+
+    // Step C. Remove old cookbook vectors
+    val qtot = vqh2.cns.map { case (_, m) => sigmoid(avgpi - m / vqh2.ntot) }.sum
+    var (berq, q, cns1, cs2) = (berp, 0, Vector.empty[(VQH#Codebook, Float)], List.empty[VQH#Codebook])
+    for (i <- 0 to vqh2.cns.length) {
+      val _cns = vqh2.cns.apply(i)
+      val (_berq, _q) = Bernoulli(sigmoid(avgpi - _cns._2 / vqh2.ntot) / qtot, berq.rng).sample
+      berq = _berq
+      q = _q
+      if (q == 0) cns1 = cns1.+:(_cns) else cs2 = _cns._1 :: cs2
+    }
+
+    (VQH(cns1, cns1.map(_._2).sum, vqh2.k, berq.rng), cnews, cs2)
+  }
 
   /**
     * @return (codebook vector, count, index)
     * */
-  def parSearch(vqh: VQH, x: INDArray, i: Int): Option[(VQH#Codebook, Float, Int)] = ???
+  def parSearch(vqh: VQH, xp: INDArray, i: Int): Option[(VQH#Codebook, Float, Int)] = ???
 
-  def expSearch(vqh: VQH, x: INDArray): Option[VQH#Codebook] = ???
+  /**
+    * @return (codebook vector, count, index)
+    * */
+  def expSearch(vqh: VQH, x: VQH#Codebook): Option[(VQH#Codebook, Float, Int)] = ???
 
   def sigmoid(x: Float): Float = 1 / (1 + math.exp(-1 * x).toFloat)
 
