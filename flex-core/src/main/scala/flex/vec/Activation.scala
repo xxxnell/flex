5796b4346e63ed5b559292f481c4d687de05d573
Nell <xxxxxnell@gmail.com>
2019-01-22 19:45:56 +0900

    + `Activation`

--- flex-core/src/main/scala/flex/vec/Activation.scala
+++ flex-core/src/main/scala/flex/vec/Activation.scala
@@ -1,24 +1,26 @@
 package flex.vec
 
+import org.nd4j.linalg.ops.transforms.Transforms
+
 trait Activation {
 
-  def step(x: Float): Float = ???
+  def step(x: Float): Float = if (x < 0) 0 else 1
 
   def sigmoid(x: Float): Float = 1 / (1 + math.exp(-1 * x).toFloat)
 
-  def tanh(x: Float): Float = ???
+  def tanh(x: Float): Float = math.tanh(x).toFloat
 
-  def relu(x: Float): Float = ???
+  def relu(x: Float): Float = if (x < 0) 0 else x
 
 }
 
 trait ActivationSyntax {
 
   implicit class ActivationVecSyntaxImpl(vec: Vec) {
-    def step: Vec = ???
-    def sigmoid: Vec = ???
-    def tanh: Vec = ???
-    def relu: Vec = ???
+    def step: Vec = Transforms.tanh(vec)
+    def sigmoid: Vec = Transforms.sigmoid(vec)
+    def tanh: Vec = Transforms.tanh(vec)
+    def relu: Vec = Transforms.relu(vec)
   }
 
 }
